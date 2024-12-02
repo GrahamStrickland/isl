cda82b066feb0a641f329ac657d389e004f389a2
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-02 18:29:58 +0200

    Add plot script for ex2_10_b.r

---
+++ src/ch02/plots/ex2_10_b.r
@@ -0,0 +1,32 @@
+# Exercise 2.10(b) - Plots of boston data
+
+library(ISLR2)
+attach(Boston)
+
+pdf("plots/ex2_10_b.pdf")
+
+par(mfrow = c(2, 2))
+plot(
+  nox,
+  dis,
+  xlab = "Nitrogen oxides concentration",
+  ylab = "Distance to employment centres"
+)
+plot(
+  dis,
+  age,
+  xlab = "Distance to employment centres",
+  ylab = "Proportion of units older than 1940"
+)
+plot(
+  rm,
+  lstat,
+  xlab = "Average rooms",
+  ylab = "Lower status of population (%)"
+)
+plot(
+  lstat,
+  medv,
+  xlab = "Lower status of population (%)",
+  ylab = "Median value of owner-occupied homes ($1000s)"
+)
