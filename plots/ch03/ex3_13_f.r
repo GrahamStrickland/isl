208873069692f3716b895aaf8738dd4db77c35b2
Graham Strickland <grahamhstrickland@gmail.com>
2025-11-06 21:27:37 +0200

    Add remainder of question 3.13

--- plots/ch03/ex3_13_f.r
+++ plots/ch03/ex3_13_f.r
@@ -1,6 +1,7 @@
 # Exercise 3.13(f) - Scatter plot for the given linear model
 # with least squares and population regression lines
 
+set.seed(1)
 x <- rnorm(100, 0, 1)
 eps <- rnorm(100, 0, 0.0625)
 y <- -1.0 + 0.5 * x + eps
