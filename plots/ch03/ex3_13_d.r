ca784ef2be3c914ce87bc4c920a673be3e33e391
Graham Strickland <grahamhstrickland@gmail.com>
2026-01-23 18:01:45 +0200

    Cut old LaTeX and plotting code

--- plots/ch03/ex3_13_d.r
+++
@@ -1,9 +0,0 @@
-# Exercise 3.13(d) - Scatter plot for the given linear model
-
-set.seed(1)
-x <- rnorm(100, 0, 1)
-eps <- rnorm(100, 0, 0.0625)
-y <- -1.0 + 0.5 * x + eps
-
-pdf("ex3_13_d.pdf")
-plot(x, y)
